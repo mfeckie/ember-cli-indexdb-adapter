30a09bcc516394c83635cb0dc8fd6be57611378a
Martin Feckie <mfeckie@gmail.com>
2015-02-12 20:15:55 +0800

    Implement migrations for adapter

--- addon/adapters/index-db.js
+++ addon/adapters/index-db.js
@@ -1,5 +1,65 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
-export default DS.RESTAdapter.extend({
+DS.IndexedDBMigration = Ember.Object.extend({
+  databaseName: '_default',
+  version: 1,
+  migrate: function () {
+    var self = this;
+
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+      var connection = indexedDB.open(self.databaseName, self.version);
+      connection.onsuccess = function () {
+        var result = this.result;
+        Ember.run(function () {
+          result.close();
+          resolve();
+        });
+      };
+
+      connection.onupgradeneeded = function (event) {
+        Ember.run(function () {
+          self.set('memoizedDatabaseForUpgrade', event.target.result);
+          self.runMigrations();
+          self.set('memoizedDatabaseForUpgrade', null);
+        });
+      };
+
+      connection.onerror = function (err) {
+        console.log('Error connecting to ', self.databaseName);
+        console.error('Error: ', err);
+        reject(err);
+      };
+    });
+  },
+  addModel: function (modelName, opts) {
+    var db = this.get('memoizedDatabaseForUpgrade');
+    var options = opts || {};
+
+    Ember.run(function () {
+      if (!db.objectStoreNames.contains(modelName)) {
+        var keyOpts = { keyPath: 'id'};
+
+        if (options.autoIncrement) {
+          keyOpts.autoIncrement = options.autoIncrement;
+        }
+
+        if (options.keyPath) {
+          keyOpts.keyPath = options.keyPath;
+        }
+
+        db.createObjectStore(modelName, keyOpts);
+      }
+    });
+  },
+  runMigrations: function () {
+    this.migrations.call(this);
+  }
+});
+
+
+
+
+export default DS.Adapter.extend({
 
 });
--- tests/unit/adapters/index-db-test.js
+++ tests/unit/adapters/index-db-test.js
@@ -1,15 +1,90 @@
 import {
   moduleFor,
   test
-} from 'ember-qunit';
+  } from 'ember-qunit';
+
+import Ember from 'ember';
+
+var databaseName = 'ember-cli-indexed-db-test-database';
+
+var App = {};
+
+var migration;
+
+var deleteDatabase = function (databaseName) {
+  var deletion = indexedDB.deleteDatabase(databaseName);
+  deletion.onsuccess = function () {
+
+  };
+  deletion.onerror = function (err) {
+    console.log('Problem deleting database:');
+    console.log(err);
+  };
+};
+
+var openDatabase = function (databaseName) {
+  return new Ember.RSVP.Promise(function (resolve, reject) {
+    var connection = indexedDB.open(databaseName);
+    connection.onsuccess = function (event) {
+      Ember.run(function () {
+        var db = connection.result;
+
+        db.onerror = function (event) {
+          Ember.run(function () {
+            console.error('Error: ', event);
+            reject(event);
+          });
+        };
+        resolve(event.target.result);
+      });
+    };
+  });
+};
 
 moduleFor('adapter:index-db', 'IndexDbAdapter', {
   // Specify the other units that are required for this test.
   // needs: ['serializer:foo']
+  setup: function () {
+    App.Person = DS.Model.extend({
+      name: DS.attr('string'),
+      cool: DS.attr('boolean'),
+      phones: DS.hasMany('phone')
+    });
+    App.Person.toString = function () { return "App.Person"; };
+
+    var TestMigration = DS.IndexedDBMigration.extend({
+      migrations: function() {
+        this.addModel(App.Person);
+      }
+    });
+
+    migration = TestMigration.create({
+      databaseName: databaseName,
+      version: 1
+    });
+    window.migration = migration;
+
+  },
+  teardown: function () {
+    deleteDatabase(databaseName);
+  }
 });
 
+var adapter;
+
 // Replace this with your real tests.
-test('it exists', function() {
-  var adapter = this.subject();
+test('it exists', function () {
+  adapter = this.subject();
   ok(adapter);
+});
+
+test('Runs migrations', function () {
+  QUnit.stop();
+  migration.migrate().then(function() {
+    return openDatabase(databaseName);
+  }).then(function(db) {
+    db.close();
+    ok(db.objectStoreNames.contains("App.Person"), "Person object store created");
+    QUnit.start();
+  });
 });
